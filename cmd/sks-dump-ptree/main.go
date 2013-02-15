093085eb1decffed6659902f5bda64ae03a4f434
Casey Marshall <casey.marshall@gmail.com>
2013-02-15 00:22:09 -0600

    Figured out SKS backwards bitstring encoding.

--- cmd/sks-dump-ptree/main.go
+++ cmd/sks-dump-ptree/main.go
@@ -74,13 +74,122 @@ func parseKey(line string) []byte {
 	if err != nil {
 		return nil
 	}
+	fmt.Printf("%x\n", buf)
 	return buf
 }
+
+type Bitstring struct {
+	buf []byte
+	bits int
+}
+
+func NewBitstring(bits int) *Bitstring {
+	n := bits / 8
+	if bits % 8 != 0 {
+		n++
+	}
+	return &Bitstring{ buf: make([]byte, n), bits: bits }
+}
+
+func (bs *Bitstring) BitLen() int {
+	return bs.bits
+}
+
+func (bs *Bitstring) ByteLen() int {
+	return len(bs.buf)
+}
+
+func (bs *Bitstring) bitIndex(bit int) (int, uint) {
+	return bit / 8, uint(bit % 8)
+}
+
+func (bs *Bitstring) Get(bit int) int {
+	bytePos, bitPos := bs.bitIndex(bit)
+	if (bs.buf[bytePos] & (byte(1)<<(8-bitPos-1))) != 0 {
+		return 1
+	}
+	return 0
+}
+
+func (bs *Bitstring) Set(bit int) {
+	bytePos, bitPos := bs.bitIndex(bit)
+	bs.buf[bytePos] |= (byte(1)<<(8-bitPos-1))
+}
+
+func (bs *Bitstring) Unset(bit int) {
+	bytePos, bitPos := bs.bitIndex(bit)
+	bs.buf[bytePos] &^= (byte(1)<<(8-bitPos-1))
+}
+
+func (bs *Bitstring) Flip(bit int) {
+	bytePos, bitPos := bs.bitIndex(bit)
+	bs.buf[bytePos] ^= (byte(1)<<(8-bitPos-1))
+}
+
+func (bs *Bitstring) SetBytes(buf []byte) {
+	for i := 0; i < len(bs.buf); i++ {
+		if i < len(buf) {
+			bs.buf[i] = buf[i]
+		} else {
+			bs.buf[i] = byte(0)
+		}
+	}
+	bytePos, bitPos := bs.bitIndex(bs.bits)
+	if bitPos != 0 {
+		mask := ^((byte(1)<<(8-bitPos))-1)
+		bs.buf[bytePos] &= mask
+	}
+}
+
+func (bs *Bitstring) Lsh(n uint) {
+	i := big.NewInt(int64(0)).SetBytes(bs.buf)
+	i.Lsh(i, n)
+	bs.SetBytes(i.Bytes())
+}
+
+func (bs *Bitstring) Rsh(n uint) {
+	i := big.NewInt(int64(0)).SetBytes(bs.buf)
+	i.Rsh(i, n)
+	bs.SetBytes(i.Bytes())
+}
+
+func (bs *Bitstring) String() string {
+	w := bytes.NewBuffer(nil)
+	for i := 0; i < bs.bits; i++ {
+		fmt.Fprintf(w, "%d", bs.Get(i))
+	}
+	return w.String()
+}
+
+func (bs *Bitstring) Bytes() []byte {
+	w := bytes.NewBuffer(nil)
+	w.Write(bs.buf)
+	return w.Bytes()
+}
+
+/*
+func (bs *Bitstring) Copy(newBits int) *Bitstring {
+	newByteLen := newBits / 8
+	if newBits % 8 != 0 {
+		newByteLen++
+	}
+	newBs := &Bitstring{ buf: make([]byte, newByteLen), bits: newBits }
+	for i := 0; i < len(bs.buf); i++ {
+		newBs.buf[i] = bs.buf[i]
+	}
+	maskBits := bs.bits % 8
+	if maskBits != 0 {
+		mask := ^(byte(1)<<uint(maskBits+1))
+		newBs.buf[len(bs.buf)-1] = bs.buf[len(bs.buf)-1] & mask
+	}
+	return newBs
+}
+*/
 
 type Node struct {
 	SValues []*Zp
 	NumElements int
-	Key *big.Int
+	Key *Bitstring
 	Leaf bool
 	Fingerprints []*Zp
 }
@@ -92,7 +201,7 @@ func (n *Node) String() string {
 		fmt.Fprintf(b, " %x", sv.Bytes())
 	}
 	fmt.Fprintf(b, "\n")
-	fmt.Fprintf(b, "Key: %x\n", n.Key)
+	fmt.Fprintf(b, "Key: %v\n", n.Key)
 	fmt.Fprintf(b, "Fingerprints:")
 	for _, fp := range n.Fingerprints {
 		fmt.Fprintf(b, " %x", fp.Bytes())
@@ -132,7 +241,8 @@ func unmarshalNode(buf []byte, bitQuantum int, numSamples int) (node *Node, err 
 	if err != nil {
 		return
 	}
-	key := big.NewInt(int64(0)).SetBytes(keyData)
+	key := NewBitstring(keyBits)
+	key.SetBytes(keyData)
 	svalues := make([]*Zp, numSamples)
 	for i := 0; i < numSamples; i++ {
 		svalues[i], err = recon.ReadZp(r)
